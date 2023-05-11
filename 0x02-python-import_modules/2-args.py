c107a3b5f38cf75572723bd813a437323c48dfce
Chidinma <chifineg@yahoo.com>
2023-05-11 14:19:50 -0700

    debugged 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-
     length = len(sys.argv) - 1
-    if length == 0:
-        print("0 arguments.")
-    elif length == 1:
+    if length == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(length))
+        print("{} arguments:".format(length), end="")
     for i in range(length):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
