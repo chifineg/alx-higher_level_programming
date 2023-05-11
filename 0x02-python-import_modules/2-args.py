9bf441a68026511b2cbc3f1f7162cd0e27d9916a
Chidinma <chifineg@yahoo.com>
2023-05-11 16:18:28 -0700

    modified 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == "__main__":
+
+
+def main():
     length = len(argv) - 1
+    word = "arguments"
+
     if length == 0:
-        print("{:d} arguments.".format(length))
-    elif length == 1:
-        print("{:d} argument:".format(length))
+        print(f"{length} {word}.")
+    elif length != 1:
+        print(f"{length} {word}:")
     else:
-        print("{:d} arguments:".format(length))
-    list_args = []
-    for i in argv:
-        list_args += [i]
-    for j in range(1, len(list_args)):
-        print("{}: {}".format(j, list_args[j]))
+        word = "argument"
+        print(f"{length} {word}:")
+
+    if length >= 1:
+        for count, args in enumerate(argv):
+            if count == 0:
+                continue
+            print(f"{count}: {args}")
+
+
+if __name__ == '__main__':
+    main()
