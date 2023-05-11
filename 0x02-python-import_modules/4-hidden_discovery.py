0758d1180452e25024efae653c4c54a484c585d0
Chidinma <chifineg@yahoo.com>
2023-05-11 15:07:56 -0700

    modified 4-hidden_discovery.py file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,6 @@ if __name__ == "__main__":
     
     import hidden_4
 
-    ident = dir(hidden_4)
-    for i in ident:
-        if i[:2] != "__":
-            print("{:s}".format(i))
+    for s in dir(hidden_4):
+        if s[:2] != "__":
+            print("{:s}".format(s))
