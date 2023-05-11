284118d004ba71d49476c24db78eec54f00d2941
Chidinma <chifineg@yahoo.com>
2023-05-11 16:14:32 -0700

    modified 0-add.py file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-
 from add_0 import add
-if __name__ == "__main__":
+
+
+def main():
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+    sum = add(a, b)
+
+    print("{:d} + {:d} = {:d}".format(a, b, sum))
+
+
+if __name__ == '__main__':
+    main()
