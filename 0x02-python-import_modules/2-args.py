a580b946a44980fdba8a2928d4c13fa5ae939346
Chidinma <chifineg@yahoo.com>
2023-05-11 12:07:07 -0700

    debugged files for tasks 0 to 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 import sys
-
 a = len(sys.argv) - 1
-
 if a == 0:
     print("0 argument.")
 elif a == 1:
     print("1 argument:")
 else:
     print("{} arguments:".format(a))
-for i in range(a + 1):
-    print("{}: {}".format(a,sys. argv[i]))
+for i in range(a):
+    print("{}: {}".format(a + 1, sys. argv[i + 1]))
