bc734c61e30d9cb5d7f289163b65107935a2214e
Chidinma <chifineg@yahoo.com>
2023-05-14 11:10:59 -0700

    debugged 0-print_list_integer function

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,7 +3,7 @@
 def print_list_integer(my_list=[]):
     length = len(my_list)
     for i in range(length):
-        print("{:s}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
 
 
 print_list_integer()
