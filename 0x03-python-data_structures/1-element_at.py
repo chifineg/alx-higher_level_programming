029299010a0c176b9f098d13858a42471b2055d3
Chidinma <chifineg@yahoo.com>
2023-05-14 12:10:35 -0700

    modified 1-element_at.py file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,6 @@
 
 def element_at(my_list, idx):
     length = len(my_list)
-    if idx < 0:
+    if idx < 0 or idx > length:
         return
-    elif idx > length:
-        return
-    else:
-        return(my_list[idx])
+    return(my_list[idx])
