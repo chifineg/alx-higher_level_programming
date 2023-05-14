1c94be9683d5ec39aa668d8bb1d6c827996d66f7
Chidinma <chifineg@yahoo.com>
2023-05-14 15:09:21 -0700

    modified 5-no_c.py file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 
 def no_c(my_string):
     length = len(my_string)
-    for i in range (length):
+    for i in range(length):
         if my_string[i] == 'c' or my_string[i] == 'C':
             my_string = my_string.translate({ord(my_string[i]): None})
             return my_string
