e89ac6c886997d2ea6d258b6c32e5eb746a49871
Chidinma <chifineg@yahoo.com>
2023-06-05 11:18:50 -0700

    added all files needed for this project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module starts here
+Defines an empty class called Rectangle
+"""
+
+
+class Rectangle:
+    """ Empty class starts here """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+For this project, we will be learning about classes in python.
+We will perform different tasks using the OOP(Object Oriented Programming) approach.
