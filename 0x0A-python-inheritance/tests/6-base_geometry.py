805cd6ae3dfeb7d8598bd219059b8c9407082c88
Chidinma <chifineg@yahoo.com>
2023-06-12 13:30:52 -0700

    copied some .py files into tests directory

---
+++ 0x0A-python-inheritance/tests/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""base geometry class called BaseGeometry."""
+
+
+class BaseGeometry:
+    """base geometry class."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
