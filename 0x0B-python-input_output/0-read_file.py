6c1557741c4fad86098c476b05d1820e84d3b19f
Chidinma <chifineg@yahoo.com>
2023-06-13 09:57:53 -0700

    modified 0-read_file.py file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """function to read from a file """
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-        print(read_data)
+        print(read_data, end="")
