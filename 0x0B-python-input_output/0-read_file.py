303802c5fd8e6afbb6fbdbbe08f99311fbd6b710
Chidinma <chifineg@yahoo.com>
2023-06-13 09:55:17 -0700

    added pycodestyle comment to 0-read_file.py file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """ program starts here """
 
+
 def read_file(filename=""):
+    """function to read from a file """
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
