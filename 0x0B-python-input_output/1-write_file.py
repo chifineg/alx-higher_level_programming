d753b488a49e0504c323be37da68365cbdea05a6
Chidinma <chifineg@yahoo.com>
2023-06-13 10:46:36 -0700

    added pycodestyle comment to 1-write_file.py file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,14 @@
 
 
 def write_file(filename="", text=""):
+    """ function to write texts to a file 
+    
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, "w", encoding="utf-8") as f:
         write_data = f.write(text)
         return write_data
